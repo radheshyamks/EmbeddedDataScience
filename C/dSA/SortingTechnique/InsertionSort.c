41804d15c101c662213dd03029716131306557f0
RadheShyam <RadheShyams51@gmail.com>
2026-03-02 13:18:16 +0530

    Added all the sorting methods file with sortMEthod.h

--- C/dSA/SortingTechnique/InsertionSort.c
+++ C/dSA/SortingTechnique/InsertionSort.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <pthread.h>
+
+#include "sortMethod.h"
 /*
 #include <assert.h>      
 #include <ctype.h>       
---
+++ C/dSA/SortingTechnique/quickSort.c
@@ -0,0 +1,1 @@
+#include "sortMethod.h"
