819e034dda78482b0e703ffacdc27dd3a62f1ba6
RadheShyam <RadheShyams51@gmail.com>
2026-03-03 23:42:22 +0530

    Modified mergeSort and header file

--- C/dSA/SortingTechnique/mergeSort.c
+++ C/dSA/SortingTechnique/mergeSort.c
@@ -1,9 +1,19 @@
 #include "sortMethod.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+typedef enum{
+    ARRAY_LENGTH=10,
+}mergeSortEnum;
+typedef struct{
+    mergeSortEnum size;
+    uint32_t Array[ARRAY_SIZE];
+}MergeSort_package;
 
 int main(int argc, char* argv[], char** env){
-    uint32_t Array[ARRAY_SIZE] = {};
+    uint32_t Array[ARRAY_SIZE] = {9,3,6,1,4,2,0,7,5,8};
+
+    for(uint8_t i=0; i<ARRAY_SIZE; i++){
+        printf("%u ", Array[i]);
+    }
     return EXIT_SUCCESS;
 }
--- C/dSA/SortingTechnique/sortMethod.h
+++ C/dSA/SortingTechnique/sortMethod.h
@@ -6,8 +6,8 @@
 // function delcaration
 void BubbleSort(uint32_t A[], uint16_t size);
 void Selection_Sort(uint32_t Arr[], uint16_t size);
-void* Insertion_Sort_(void* arg);
-void Insertion_Sort(uint32_t Arr[], uint16_t size);
+void* Insertion_Sort(void* arg);
+// void Insertion_Sort(uint32_t Arr[], uint16_t size);
 void mergeSort(uint32_t Arr[], uint16_t size);
 void mergeSort_Recursion(uint32_t Arr[], uint16_t left, uint16_t right);
 void mergeSorted_Array(uint32_t Arr[], uint16_t left, uint16_t mid, uint16_t right);
