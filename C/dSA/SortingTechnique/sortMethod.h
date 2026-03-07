50be2654f749adce8a7e3635d2440dfef96c2d2d
RadheShyam <RadheShyams51@gmail.com>
2026-03-07 14:29:20 +0530

    modified all files

--- C/dSA/SortingTechnique/sortMethod.h
+++ C/dSA/SortingTechnique/sortMethod.h
@@ -8,7 +8,13 @@ void BubbleSort(uint32_t A[], uint16_t size);
 void Selection_Sort(uint32_t Arr[], uint16_t size);
 void* Insertion_Sort(void* arg);
 // void Insertion_Sort(uint32_t Arr[], uint16_t size);
+/* merge sort */
 void mergeSort(uint32_t Arr[], uint16_t size);
-void mergeSort_Recursion(uint32_t Arr[], uint16_t left, uint16_t right);
+void mergeSort_Recursion(uint32_t Arr[], uint16_t left_most, uint16_t right_most);
 void mergeSorted_Array(uint32_t Arr[], uint16_t left, uint16_t mid, uint16_t right);
+/* Quick Sort */
+void QuickSort(uint32_t Arr[], uint16_t size);
+void swap(uint32_t* val1, uint32_t* val2);
+uint16_t partition(uint32_t Arr[], uint16_t low, uint16_t high);
+void QuickSort_recur(uint32_t Arr[], uint16_t low, uint16_t high);
 #endif
